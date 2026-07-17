e11f602903be29292b819ed724b277934ae6b041
Juntao Tan <juntao.tan.it@gmail.com>
2026-07-17 23:26:18 +1200

    chore: initialise project structure - add Spring Boot backend - add Docker Compose configuration - configure Git ignore rules
    Next, initialise backend structure

---
+++ backend/src/main/java/dev/margintrace/margin_attribution_backend/MarginAttributionBackendApplication.java
@@ -0,0 +1,13 @@
+package dev.margintrace.margin_attribution_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarginAttributionBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarginAttributionBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/dev/margintrace/margin_attribution_backend/MarginAttributionBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.margintrace.margin_attribution_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarginAttributionBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
