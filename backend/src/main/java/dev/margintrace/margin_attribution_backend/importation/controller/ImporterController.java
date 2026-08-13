4381ae27971c0fc8fcbd94d7bbef30889c02ae94
tanjuntao-dev <juntao.tan.it@gmail.com>
2026-08-13 15:06:10 +1200

    feat(controller): complete the import controller - add "/imports" router to accept a file to be imported

---
+++ backend/src/main/java/dev/margintrace/margin_attribution_backend/importation/controller/ImporterController.java
@@ -0,0 +1,33 @@
+package dev.margintrace.margin_attribution_backend.importation.controller;
+
+import dev.margintrace.margin_attribution_backend.importation.context.ImportContext;
+import dev.margintrace.margin_attribution_backend.importation.service.ImportService;
+import dev.margintrace.margin_attribution_backend.importation.service.ImportWorkflow;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/imports")
+@RequiredArgsConstructor
+public class ImporterController {
+    private final ImportService importService;
+    private final ImportWorkflow importWorkflow;
+
+    @PostMapping(consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<Void> importFile(@RequestParam("file") MultipartFile file) {
+        // Initialise import job's context
+        ImportContext context = new ImportContext();
+        context.setFile(file);
+        // Initialise import job id
+        long jobId = importService.importer(file);
+        importWorkflow.execute(jobId, context);
+
+        return ResponseEntity.ok().build();
+    }
+}
